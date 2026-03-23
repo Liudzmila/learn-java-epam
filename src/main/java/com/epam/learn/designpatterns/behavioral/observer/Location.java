a3736d3d6293d58cb8d7c35d6198d4d039e54bf2
Liudzmila Sobaleva <lusya88@gmail.com>
2026-03-23 21:19:10 +0100

    Observer pattern template

---
+++ src/main/java/com/epam/learn/designpatterns/behavioral/observer/Location.java
@@ -0,0 +1,18 @@
+package main.java.com.epam.learn.designpatterns.behavioral.observer;
+
+import java.beans.PropertyChangeSupport;
+
+public class Location {
+
+    private String weatherEvent;
+    private PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public void setWeatherEvent(String weatherEvent) {
+        support.firePropertyChange("weatherEvent", this.weatherEvent, weatherEvent);
+        this.weatherEvent = weatherEvent;
+    }
+
+    public void addPropertyChangeListener(WeatherUpdates weatherUpdates) {
+        support.addPropertyChangeListener(weatherUpdates);
+    }
+}
---
+++ src/main/java/com/epam/learn/designpatterns/behavioral/observer/WeatherUpdates.java
@@ -0,0 +1,20 @@
+package main.java.com.epam.learn.designpatterns.behavioral.observer;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeatherUpdates implements PropertyChangeListener {
+
+    private List<String> weatherUpdates = new ArrayList<>();
+
+    public void printUpdates() {
+        weatherUpdates.forEach(System.out::println);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        weatherUpdates.add((String) evt.getNewValue());
+    }
+}
