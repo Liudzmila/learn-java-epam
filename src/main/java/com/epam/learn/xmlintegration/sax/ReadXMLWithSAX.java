997c366061af888b6e0d1c8b980035d3d9605d82
liudzmilasobaleva <lusya88@gmail.com>
2026-04-02 17:59:52 +0200

    Parsing XML with SAX (Simple API for XML) example

---
+++ src/main/java/com/epam/learn/xmlintegration/sax/ReadXMLWithSAX.java
@@ -0,0 +1,22 @@
+package main.java.com.epam.learn.xmlintegration.sax;
+
+import main.java.com.epam.learn.xmlintegration.dataprovider.DataProvider;
+
+import java.util.List;
+
+public class ReadXMLWithSAX {
+
+	public static void main(String[] args) throws Exception {
+
+		String filename = DataProvider.DATADIR + "customersError.xml";
+		
+		SAXCustomerHandler saxHandler = new SAXCustomerHandler();
+		List<Customer> data = saxHandler.readDataFromXML(filename);
+		System.out.println("Number of customers: " + data.size());
+		
+		for (Customer customer : data) {
+			System.out.println(customer);
+		}
+	}
+
+}
