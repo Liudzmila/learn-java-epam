7424e1b6c00914085f6c31ba5ba98bd0fa22e51a
liudzmilasobaleva <lusya88@gmail.com>
2026-04-03 19:42:47 +0200

    reading XML with DOM (Document Object Model)

---
+++ src/main/java/com/epam/learn/xmlintegration/api/dom/read/DOMReader.java
@@ -0,0 +1,79 @@
+package main.java.com.epam.learn.xmlintegration.api.dom.read;
+
+import main.java.com.epam.learn.xmlintegration.model.Customer;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DOMReader {
+	
+	private static final String XMLDATEFORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+
+	public List<Customer> getDataFromXML(String filename, String filter) throws XPathExpressionException {
+		
+		List<Customer> data = new ArrayList<>();
+
+		File xmlFile = new File(filename);
+		Document doc = null;
+		
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			doc = builder.parse(xmlFile);
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
+
+		XPathFactory xFactory = XPathFactory.newInstance();
+        XPath xPath = xFactory.newXPath();
+		XPathExpression expression = xPath.compile(filter);
+		NodeList list = (NodeList) expression.evaluate(doc, XPathConstants.NODESET);
+
+//		NodeList list = doc.getElementsByTagName("customer");
+		System.out.println("Nodes found: " + list.getLength());
+		
+		for (int i = 0; i < list.getLength(); i++) {
+			Customer customer = new Customer();
+			data.add(customer);
+			
+			Element custElement = (Element) list.item(i);
+			String idAsString = custElement.getAttribute(Customer.ID);
+			customer.setId(Integer.parseInt(idAsString));
+
+			String content = getTextFromElement(custElement, Customer.NAME);
+			customer.setName(content);
+			
+			String joined = getTextFromElement(custElement, Customer.JOINED);
+			DateFormat df = new SimpleDateFormat(XMLDATEFORMAT);
+			try {
+				customer.setJoined(df.parse(joined));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		return data;
+		
+	}
+
+	private String getTextFromElement(Element custElement, String elementName) {
+		Element node = (Element) custElement.getElementsByTagName(elementName).item(0);
+		String content = node.getTextContent();
+		return content;
+	}
+
+}
---
+++ src/main/java/com/epam/learn/xmlintegration/api/dom/read/ReadXMLWithDOM.java
@@ -0,0 +1,24 @@
+package main.java.com.epam.learn.xmlintegration.api.dom.read;
+
+import main.java.com.epam.learn.xmlintegration.dataprovider.DataProvider;
+import main.java.com.epam.learn.xmlintegration.model.Customer;
+
+import javax.xml.xpath.XPathExpressionException;
+import java.util.List;
+
+public class ReadXMLWithDOM {
+
+	public static void main(String[] args) throws XPathExpressionException {
+
+		DOMReader reader = new DOMReader();
+		
+		List<Customer> data = reader.getDataFromXML(DataProvider.DATADIR + "customers.xml",
+				"//customer[age >= 65]");
+		System.out.println("There are " + data.size() + " records");
+		
+		for (Customer customer : data) {
+			System.out.println(customer);
+		}
+	}
+
+}
