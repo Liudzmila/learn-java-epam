4c248bd1e14fa72c659e50037272c1d5f2526913
liudzmilasobaleva <lusya88@gmail.com>
2026-04-04 17:43:34 +0200

    Creating XML with JAXB

---
+++ src/main/java/com/epam/learn/xmlintegration/api/jaxb/create/CreateXMLWithJAXB.java
@@ -0,0 +1,41 @@
+package main.java.com.epam.learn.xmlintegration.api.jaxb.create;
+
+import main.java.com.epam.learn.xmlintegration.api.jaxb.create.dataprovider.DataProvider;
+import main.java.com.epam.learn.xmlintegration.api.jaxb.create.model.Customer;
+import main.java.com.epam.learn.xmlintegration.api.jaxb.create.model.Customers;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.List;
+
+public class CreateXMLWithJAXB {
+
+	public static void main(String[] args) throws IOException {
+
+		List<Customer> data = DataProvider.getData(DataProvider.SMALL);
+
+		Customers customers = new Customers();
+		customers.setCustomers(data);
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(Customers.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            StringWriter sw = new StringWriter();
+            marshaller.marshal(customers, sw);
+
+            File f = new File("./out/customers.xml");
+            marshaller.marshal(customers, f);
+
+            System.out.println(sw.toString());
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/com/epam/learn/xmlintegration/api/jaxb/create/model/Customers.java
@@ -0,0 +1,27 @@
+package main.java.com.epam.learn.xmlintegration.api.jaxb.create.model;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+import java.util.List;
+
+
+
+@XmlRootElement(name="customers")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Customers {
+
+	@XmlElement(name="customer", type=Customer.class)
+	private List<Customer> customers;
+
+	public List<Customer> getCustomers() {
+		return customers;
+	}
+
+	public void setCustomers(List<Customer> customers) {
+		this.customers = customers;
+	}
+		
+}
